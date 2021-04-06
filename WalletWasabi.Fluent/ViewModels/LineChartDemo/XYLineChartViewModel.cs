57f50eb4131fd4009e311b7f6c975510a0b71a39
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-06 18:04:35 +0200

    Line chart demo for DateTime

--- WalletWasabi.Fluent/ViewModels/LineChartDemo/XYLineChartViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/LineChartDemo/XYLineChartViewModel.cs
@@ -6,32 +6,47 @@ namespace WalletWasabi.Fluent.ViewModels.LineChartDemo
 {
 	public class XYLineChartViewModel
 	{
-		public List<double> XAxisValues => new()
+		private static readonly DateTime TimeOrigin = new DateTime(1899, 12, 31, 0, 0, 0, DateTimeKind.Utc);
+
+		public static double ToDouble(DateTime value)
 		{
-			1008,
-			432,
-			144,
-			72,
-			36,
-			18,
-			6,
-			3,
-			2,
-			1,
-		};
+			var span = value - TimeOrigin;
+			return span.TotalDays + 1;
+		}
+
+		public XYLineChartViewModel()
+		{
+			var values = new List<DateTime>()
+			{
+				new DateTime(2021, 4, 1, 0, 0, 0),
+				new DateTime(2021, 4, 1, 1, 0, 0),
+				new DateTime(2021, 4, 1, 2, 0, 0),
+				new DateTime(2021, 4, 1, 3, 0, 0),
+				new DateTime(2021, 4, 1, 4, 0, 0),
+			};
+
+			XAxisValues = new();
+			XAxisLabels = new();
+
+			foreach (var value in values)
+			{
+				var d = ToDouble(value);
+				XAxisValues.Add(d);
+				XAxisLabels.Add(value.ToString());
+			}
+		}
+
+		public List<double> XAxisValues { get; }
+
+		public List<string> XAxisLabels { get; }
 
 		public List<double> YAxisValues => new()
 		{
 			4,
-			4,
-			7,
-			22,
-			57,
-			97,
-			102,
-			123,
-			123,
-			185
+			2,
+			5,
+			8,
+			1
 		};
 	}
 }
