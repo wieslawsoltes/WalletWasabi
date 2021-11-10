ae817fc11ab8cfcc0cbefbda652958e2af5e46e9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-11-10 20:29:33 +0100

    Revert "Add debug code"
    This reverts commit e03c7691cc0ebeabce8ff79b844fb191ef852e5b.

--- WalletWasabi.Fluent/Controls/Lazy.cs
+++ WalletWasabi.Fluent/Controls/Lazy.cs
@@ -180,9 +180,6 @@ namespace WalletWasabi.Fluent.Controls
 			base.OnDetachedFromVisualTree(e);
 		}
 
-		private static volatile int s_countLoaded = 0;
-		private static volatile int s_countUnloaded = 0;
-
 		private void LoadChild()
 		{
 			if (Type == null ||
@@ -202,7 +199,6 @@ namespace WalletWasabi.Fluent.Controls
 			IsLoadInProgress = false;
 			if (Child != null)
 			{
-				Console.WriteLine($"[LOADED] {Child} {s_countLoaded++}");
 				Child.IsVisible = true;
 			}
 		}
@@ -245,15 +241,10 @@ namespace WalletWasabi.Fluent.Controls
 			if (!ReferenceEquals(task, _currentUnloadWaitTask) ||
 			    Child == null)
 			{
-				Console.WriteLine($"[UNLOADED] {Child} {s_countUnloaded++}");
 				return;
 			}
 
-			Dispatcher.UIThread.Post(() =>
-			{
-				Console.WriteLine($"[UNLOADED] {Child} {s_countUnloaded++}");
-				Child = null;
-			});
+			Dispatcher.UIThread.Post(() => Child = null);
 		}
 
 		protected override Size MeasureOverride(Size availableSize)
