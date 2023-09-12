292f6ceaf420f547bcb994a11b1193322fd3116d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-12 13:09:39 +0200

    Add ViewLocator caching for controls

--- WalletWasabi.Fluent/ViewLocator.cs
+++ WalletWasabi.Fluent/ViewLocator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using WalletWasabi.Fluent.ViewModels;
@@ -7,12 +8,29 @@ namespace WalletWasabi.Fluent;
 [StaticViewLocator]
 public partial class ViewLocator : IDataTemplate
 {
+	private readonly Dictionary<object, Control> _cache = new();
+
 	public IControl Build(object data)
 	{
 		var type = data.GetType();
+
+		Control? control;
+		if (_cache.TryGetValue(data, out control))
+		{
+#if DEBUG
+			Console.WriteLine($"[CACHED] '{data}'='{control}'");
+#endif
+			return control;
+		}
+
 		if (s_views.TryGetValue(type, out var func))
 		{
-			return func.Invoke();
+			control = func.Invoke();
+			_cache[data] = control;
+#if DEBUG
+			Console.WriteLine($"[ADD] '{data}'='{control}'");
+#endif
+			return control;
 		}
 		throw new Exception($"Unable to create view for type: {type}");
 	}
