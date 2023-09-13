3bb555e6445079d90f7d04d69d335c580cc1c280
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-09-13 13:49:39 +0200

    Try Hind2 font

--- WalletWasabi.Fluent.Desktop/Extensions/AppBuilderExtension.cs
+++ WalletWasabi.Fluent.Desktop/Extensions/AppBuilderExtension.cs
@@ -3,11 +3,30 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Dialogs;
 using Avalonia.Media;
+using Avalonia.Media.Fonts;
 using Avalonia.Platform;
 using WalletWasabi.Logging;
 
 namespace WalletWasabi.Fluent.Desktop.Extensions;
 
+public sealed class Hind2FontCollection : EmbeddedFontCollection
+{
+	public Hind2FontCollection() : base(
+		new Uri("fonts:Hind2", UriKind.Absolute),
+		new Uri("avares://WalletWasabi.Fluent/Assets/Fonts", UriKind.Absolute))
+	{
+	}
+}
+public static class AppBuilderHind2FontExtension
+{
+	public static AppBuilder WithHind2Font(this AppBuilder appBuilder)
+	{
+		return appBuilder.ConfigureFonts(fontManager =>
+		{
+			fontManager.AddFontCollection(new Hind2FontCollection());
+		});
+	}
+}
 public static class AppBuilderExtension
 {
 	public static AppBuilder SetupAppBuilder(this AppBuilder appBuilder)
@@ -32,7 +51,9 @@ public static class AppBuilderExtension
 
 		return appBuilder
 			.WithInterFont()
-			.With(new FontManagerOptions { DefaultFamilyName = "fonts:Inter#Inter, $Default" })
+			// .With(new FontManagerOptions { DefaultFamilyName = "fonts:Inter#Inter, $Default" })
+			.WithHind2Font()
+			.With(new FontManagerOptions { DefaultFamilyName = "fonts:Hind2#Hind2, $Default" })
 			.With(new SkiaOptions { MaxGpuResourceSizeBytes = 2560 * 1600 * 4 * 12 })
 			.With(new Win32PlatformOptions
 			{
