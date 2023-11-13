3200677827466c4af95bf1cf07e7f22e235db076
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-11-13 11:01:24 +0100

    Add TDG debug code

--- WalletWasabi.Fluent/Views/Wallets/Home/History/HistoryTable.axaml.cs
+++ WalletWasabi.Fluent/Views/Wallets/Home/History/HistoryTable.axaml.cs
@@ -1,7 +1,42 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace WalletWasabi.Fluent.Views.Wallets.Home.History;
+
+public class TreeDataGridEx : Avalonia.Controls.TreeDataGrid
+{
+	protected override Type StyleKeyOverride => typeof(Avalonia.Controls.TreeDataGrid);
+
+	protected override Size MeasureOverride(Size availableSize)
+	{
+		try
+		{
+			return base.MeasureOverride(availableSize);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+
+		}
+		return default;
+	}
+
+	protected override Size ArrangeOverride(Size finalSize)
+	{
+		try
+		{
+			return base.ArrangeOverride(finalSize);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+
+		}
+
+		return default;
+	}
+}
 
 public class HistoryTable : UserControl
 {
